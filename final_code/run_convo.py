8f4f59ef1962eec2f44826b7218121aafab7fbed
Aidan Housenbold <HousenboldA@gmail.com>
2026-02-16 14:59:32 -0500

    working complete eval and recording

--- final_code/run_convo.py
+++ final_code/run_convo.py
@@ -8,14 +8,17 @@ from langchain_core.messages import HumanMessage
 import uuid
 import pandas as pd
 import json
+import csv
+from tqdm import tqdm
+from datetime import datetime
 
 DEBUG = True
 
-def run_conversation(persona: tuple[CasePersona, WarningSigns], therapist, max_turns: int = 10, streaming: bool=False):
-    run_id = uuid.uuid4().hex[:8]
+def run_conversation(run_id: int, persona_id: int, persona: tuple[CasePersona, WarningSigns],therapist, max_turns: int = 10, streaming: bool=False):
+    instance_id = uuid.uuid4().hex[:8]
 
-    persona_config = {"configurable": {"thread_id": f"persona-{run_id}"}}
-    therapist_config = {"configurable": {"thread_id": f"therapist-{run_id}"}}
+    persona_config = {"configurable": {"thread_id": f"persona-{instance_id}"}}
+    therapist_config = {"configurable": {"thread_id": f"therapist-{instance_id}"}}
     readable_convo = []
 
     #Set the flag to fill out the form before
@@ -85,6 +88,35 @@ def run_conversation(persona: tuple[CasePersona, WarningSigns], therapist, max_t
         print(therapist_response['case'])
         print("="*35)
     
+    #Save conversation, SAD, warning signs, 
+    #conversation
+    write_row("runs/conversations.csv", {
+        "run_id": run_id,
+        "persona_id": persona_id,
+        "readable_conversation": readable_convo,
+        "persona_reasoning_trace": patient_response["CoT_traces"]
+    })
+
+    #SAD
+    write_row("runs/sad_scores.csv", {
+        "run_id": run_id,
+        "persona_id": persona_id,
+        "initial_survey": patient_response["intake_SAD_total"],
+        "exit_survey": patient_response["exit_SAD_total"]
+    })
+
+    #warning_signs
+    write_row("runs/warning_signs.csv", {
+        "run_id": run_id,
+        "persona_id": persona_id,
+        "before_hopelessness_intensity": persona[1].hopelessnes_intenisty,
+        "before_negative_core_belief_intensity": persona[1].negative_core_belief_intensity,
+        "before_distress_tolerance_intensity": persona[1].distress_tolerance_intensity,
+        "after_hopelessness_intensity": patient_response["warning_signs"].hopelessnes_intenisty,
+        "after_negative_core_belief_intensity": patient_response["warning_signs"].negative_core_belief_intensity,
+        "after_distress_tolerance_intensity": patient_response["warning_signs"].distress_tolerance_intensity,
+    })
+    
     #Judge the outputs
     #judge the fidelity
     fidelity_judge_state: FidelityJudgeState = {
@@ -102,18 +134,63 @@ def run_conversation(persona: tuple[CasePersona, WarningSigns], therapist, max_t
         "ground_truth_case": patient_response['persona'].model_dump(),
         "conceptualized_case": therapist_response['case'].model_dump()
     }
-    case_judge_app.invoke(concept_eval)
+    case_judge_response = case_judge_app.invoke(concept_eval)
 
-def test_crisis_categorization(Therapist):
+    write_row("runs/fidelity.csv", {
+        "run_id": run_id,
+        "persona_id": persona_id,
+        "ctrs_total": fidelity_judge_response['rating'].total_score,
+        "ctrs_part_i": fidelity_judge_response['rating'].part_i_score,
+        "ctrs_part_ii": fidelity_judge_response['rating'].part_ii_score,
+        "agenda": fidelity_judge_response['rating'].agenda,
+        "agenda_reasoning": fidelity_judge_response['rating'].agenda_reasoning,
+        "feedback": fidelity_judge_response['rating'].feedback,
+        "feedback_reasoning": fidelity_judge_response['rating'].feedback_reasoning,
+        "understanding": fidelity_judge_response['rating'].understanding,
+        "understanding_reasoning": fidelity_judge_response['rating'].understanding_reasoning,
+        "interpersonal_effectiveness": fidelity_judge_response['rating'].interpersonal_effectiveness,
+        "interpersonal_effectiveness_reasoning": fidelity_judge_response['rating'].interpersonal_effectiveness_reasoning,
+        "collaboration": fidelity_judge_response['rating'].collaboration,
+        "collaboration_reasoning": fidelity_judge_response['rating'].collaboration_reasoning,
+        "pacing_and_time_use": fidelity_judge_response['rating'].pacing_and_time_use,
+        "pacing_and_time_use_reasoning": fidelity_judge_response['rating'].pacing_and_time_use_reasoning,
+        "guided_discovery": fidelity_judge_response['rating'].guided_discovery,
+        "guided_discovery_reasoning": fidelity_judge_response['rating'].guided_discovery_reasoning,
+        "focusing_on_key_cognitions": fidelity_judge_response['rating'].focusing_on_key_cognitions,
+        "focusing_on_key_cognitions_reasoning": fidelity_judge_response['rating'].focusing_on_key_cognitions_reasoning,
+        "strategy_for_change": fidelity_judge_response['rating'].strategy_for_change,
+        "strategy_for_change_reasoning": fidelity_judge_response['rating'].strategy_for_change_reasoning,
+        "application_of_techniques": fidelity_judge_response['rating'].application_of_techniques,
+        "application_of_techniques_reasoning": fidelity_judge_response['rating'].application_of_techniques_reasoning,
+        "homework": fidelity_judge_response['rating'].homework,
+        "homework_reasoning": fidelity_judge_response['rating'].homework_reasoning,
+    })
 
+    #case_conceptualization
+    write_row("runs/conceptualization.csv", {
+        "run_id": run_id,
+        "persona_id": persona_id,
+        "concept_total": case_judge_response['rating'].total,
+        "situation": case_judge_response['rating'].situation,
+        "situation_reasoning": case_judge_response['rating'].situation_reasoning,
+        "automatic_thoughts": case_judge_response['rating'].automatic_thoughts,
+        "automatic_thoughts_reasoning": case_judge_response['rating'].automatic_thoughts_reasoning,
+        "cognitive_distortions": case_judge_response['rating'].cognitive_distortions,
+        "cognitive_distortions_reasoning": case_judge_response['rating'].cognitive_distortions_reasoning,
+        "emotions": case_judge_response['rating'].emotions,
+        "emotions_reasoning": case_judge_response['rating'].emotions_reasoning,
+        "behaviors": case_judge_response['rating'].behaviors,
+        "behaviors_reasoning": case_judge_response['rating'].behaviors_reasoning,
+    })
+
+def test_crisis_categorization(run_id: int, Therapist):
     #get the reddit cases
     with open("acute_crisis/crisis_messages.json", "r") as f:
         crisis_cases = json.load(f)
 
-    results = []
-    for case in crisis_cases["crisis_dataset"][:1]:
-        run_id = uuid.uuid4().hex[:8]
-        therapist_config = {"configurable": {"thread_id": f"therapist-{run_id}"}}
+    for case in tqdm(crisis_cases["crisis_dataset"], desc="Running crisis evals"):
+        instance_id = uuid.uuid4().hex[:8]
+        therapist_config = {"configurable": {"thread_id": f"therapist-{instance_id}"}}
 
         classification = base_app.invoke({
             "messages":[HumanMessage(content=case["message"])],
@@ -130,7 +207,8 @@ def test_crisis_categorization(Therapist):
             evaluation = crisis_judge_app.invoke({
                 "response": response["messages"][-1].content
             })
-            results.append({
+            write_row("runs/crisis.csv", {
+                "run_id": run_id,
                 "case_id": case["id"],
                 "expected": case["crisis_type"],
                 "actual": classification["crisis_classification"].classification,
@@ -139,7 +217,7 @@ def test_crisis_categorization(Therapist):
                 "therapist_reasoning": response["reasoning_traces"],
                 "eval_score": evaluation["total"],
                 "assess": evaluation["rating"].assess,                           
-                "assess_reasoning": evaluation["rating"].asses_reasoning,       
+                "assess_reasoning": evaluation["rating"].assess_reasoning,       
                 "de_escalate": evaluation["rating"].de_escalate,
                 "de_escalate_reasoning": evaluation["rating"].de_escalate_reasoning,
                 "recommend_emergency_services": evaluation["rating"].recommend_emergency_services,
@@ -148,17 +226,35 @@ def test_crisis_categorization(Therapist):
                 "request_human_consultation_reasoning": evaluation["rating"].request_human_consultation_reasoning
             })
         else:
-            results.append({
+            write_row("runs/crisis.csv",{
+                "run_id": run_id,
                 "case_id": case["id"],
                 "expected": case["crisis_type"],
                 "actual": classification["crisis_classification"].classification,
                 "reasoning": classification["crisis_classification"].reasoning,
             })
 
-    print(results)
-    df = pd.DataFrame(results)
-    print(df)
+def write_row(filename: str, row: dict):
+    cleaned = {k: v.replace('\n', ' ').strip() if isinstance(v, str) else v for k, v in row.items()}
+    with open(filename, "r") as f:
+        fieldnames = f.readline().strip().split(",")
+    
+    with open(filename, "a", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames, restval="")
+        writer.writerow(cleaned)
 
 if __name__ == "__main__":
-    run_conversation(persona_list[0], base_app, 5, True)
-    test_crisis_categorization(base_app)
+    run_id = uuid.uuid4().hex[:8]
+    num_rounds = 10
+    therapist = "baseline" 
+    date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    write_row("runs/run_meta_data.csv", {
+        "run_id": run_id,
+        "num_rounds": num_rounds,
+        "therapist": therapist,
+        "run_date_time": date_time
+    })
+
+    run_conversation(run_id, 0, persona_list[0], base_app, 5, True)
+    test_crisis_categorization(run_id, base_app)
